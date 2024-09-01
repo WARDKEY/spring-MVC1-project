123faf2d5ca862eaf80d1e97818ef747a040ec5b
WARDKEY <tjrmsduq@naver.com>
2024-09-01 15:26:02 +0900

    HTTP 요청 메시지 - JSON

--- src/main/java/hello/springmvc/basic/request/RequestBodyStringController.java
+++ src/main/java/hello/springmvc/basic/request/RequestBodyStringController.java
@@ -21,10 +21,6 @@ import lombok.extern.slf4j.Slf4j;
 @Controller
 public class RequestBodyStringController {
 
-	/**
-	 * InputStream(Reader): HTTP 요청 메시지 바디의 내용을 직접 조회
-	 * * OutputStream(Writer): HTTP 응답 메시지의 바디에 직접 결과 출력
-	 */
 	@PostMapping("/request-body-string-v1")
 	public void requestBodyString(HttpServletRequest request, HttpServletResponse response) throws IOException {
 		// 스트림은 항상 바이트코드이며 바이트코드로 문자를 받을 경우 어떤 인코디으로 문자를 받을지 지정해줘야 함
@@ -37,13 +33,9 @@ public class RequestBodyStringController {
 	}
 
 	/**
-	 * HttpEntity: HTTP header, body 정보를 편리하게 조회
-	 * - 메시지 바디 정보를 직접 조회(@RequestParam X, @ModelAttribute X)
-	 * - HttpMessageConverter 사용 -> StringHttpMessageConverter 적용 *
-	 * 응답에서도 HttpEntity 사용 가능
-	 * - 메시지 바디 정보 직접 반환(view 조회X)
-	 * - HttpMessageConverter 사용 -> StringHttpMessageConverter 적용
-	 * */
+	 * InputStream(Reader): HTTP 요청 메시지 바디의 내용을 직접 조회
+	 * * OutputStream(Writer): HTTP 응답 메시지의 바디에 직접 결과 출력
+	 */
 	@PostMapping("/request-body-string-v2")
 	public void requestBodyStringV2(InputStream inputStream, Writer responseWriter) throws IOException {
 
@@ -52,6 +44,14 @@ public class RequestBodyStringController {
 		responseWriter.write("ok");
 	}
 
+	/**
+	 * HttpEntity: HTTP header, body 정보를 편리하게 조회
+	 * - 메시지 바디 정보를 직접 조회(@RequestParam X, @ModelAttribute X)
+	 * - HttpMessageConverter 사용 -> StringHttpMessageConverter 적용 *
+	 * 응답에서도 HttpEntity 사용 가능
+	 * - 메시지 바디 정보 직접 반환(view 조회X)
+	 * - HttpMessageConverter 사용 -> StringHttpMessageConverter 적용
+	 * */
 	@PostMapping("/request-body-string-v3")
 	public HttpEntity<String> requestBodyStringV3(HttpEntity<String> httpEntity) throws IOException {
 		// HttpMessageConverter가 동작하여 httpBody에 있는 것을  String으로 바꿔줌
